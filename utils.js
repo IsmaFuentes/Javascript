3764bdd448e12c9bcd58c1e4331aa653b856b09d
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2019-09-18 12:16:05 +0200

    Add files via upload

--- utils.js
+++ utils.js
@@ -28,3 +28,62 @@ function includeHTML() {
         }
     }
 }
+
+function GetById(id) {
+    var object = document.getElementById(id);
+
+    return object;
+}
+
+function GetByClass(id) {
+    var object = document.getElementsByClassName(id);
+
+    return object;
+}
+
+function get(id, type) {
+
+    switch (type) {
+        case "id":
+            return object = document.getElementById(id);
+        case "class":
+            return object = document.getElementsByClassName(id);
+        case "tag":
+            return object = document.getElementsByTagName(id);
+        case "name":
+            return object = document.getElementsByName(id);
+    }
+}
+
+function valueOf(object) {
+    return object.value;
+}
+
+var container = {
+    main: GetById("main"),
+    navbar: GetById("navbar"),
+    body: GetById("body"),
+    footer: GetById("footer")
+};
+
+function AppendTo(id, document) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            GetById(id).innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", document, true);
+    xhttp.send();
+}
+
+function Load(object, document) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            object.innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", document, true);
+    xhttp.send();
+}
